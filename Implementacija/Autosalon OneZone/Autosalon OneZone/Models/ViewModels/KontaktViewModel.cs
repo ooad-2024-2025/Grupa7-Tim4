9bb1a148d64669c98344047ee87ace9e0ffb96a5
hhadzicc <105018985+hhadzicc@users.noreply.github.com>
2025-05-20 03:37:04 +0200

    Upiti za podrsku i prikaz upita {problem2 ostaje}

---
+++ Implementacija/Autosalon OneZone/Autosalon OneZone/Models/ViewModels/KontaktViewModel.cs
@@ -0,0 +1,15 @@
+﻿// Autosalon OneZone/Models/ViewModels/KontaktViewModel.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace Autosalon_OneZone.ViewModels
+{
+    public class KontaktViewModel
+    {
+        [Required(ErrorMessage = "Naslov je obavezan")]
+        [MaxLength(200, ErrorMessage = "Naslov ne može biti duži od 200 karaktera")]
+        public string Naslov { get; set; }
+
+        [Required(ErrorMessage = "Sadržaj poruke je obavezan")]
+        public string Sadrzaj { get; set; }
+    }
+}
