c6e9f9895a1a82b15b7c577eb98138cefceb0f11
hhadzicc <105018985+hhadzicc@users.noreply.github.com>
2025-05-21 00:58:23 +0200

    Fixani stilovi

--- Implementacija/Autosalon OneZone/Autosalon OneZone/obj/Debug/net8.0/Autosalon OneZone.AssemblyInfo.cs
+++ Implementacija/Autosalon OneZone/Autosalon OneZone/obj/Debug/net8.0/Autosalon OneZone.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Autosalon OneZone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c22afd0dfa4d3fd0973ef85590234036d569b17c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b7115c8e546015c01091ca728b256379a55c43")]
 [assembly: System.Reflection.AssemblyProductAttribute("Autosalon OneZone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Autosalon OneZone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
