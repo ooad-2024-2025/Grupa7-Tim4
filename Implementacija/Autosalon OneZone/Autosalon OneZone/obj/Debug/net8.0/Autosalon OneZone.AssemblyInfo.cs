292f0c55f8a64bc55b481552a99ccdedd5c35cdb
hhadzicc <105018985+hhadzicc@users.noreply.github.com>
2025-05-29 21:31:44 +0200

    Checkout fix. sada dobro radi StavkeKorpe i sumiranje Narudzbe a Placanja ne treba

--- Implementacija/Autosalon OneZone/Autosalon OneZone/obj/Debug/net8.0/Autosalon OneZone.AssemblyInfo.cs
+++ Implementacija/Autosalon OneZone/Autosalon OneZone/obj/Debug/net8.0/Autosalon OneZone.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Autosalon OneZone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e5be17840fc8ce9527ced8c1612bf5afa8e90ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b8cda230c7e81c24d3584b58c16b73e97f420e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Autosalon OneZone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Autosalon OneZone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
