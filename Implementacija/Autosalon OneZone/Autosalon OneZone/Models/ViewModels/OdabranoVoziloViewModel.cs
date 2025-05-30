69588d8ef827ec973e19a245daa5ade93bcadb10
hhadzicc <hhadzic4@gmail.com>
2025-05-30 11:09:47 +0200

    fix1

---
+++ Implementacija/Autosalon OneZone/Autosalon OneZone/Models/ViewModels/OdabranoVoziloViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Autosalon_OneZone.Models.ViewModels
+{
+    // Može se dodati u Models/ViewModels/OdabranoVoziloViewModel.cs
+    public class OdabranoVoziloViewModel
+    {
+        public int id { get; set; }
+        public string naziv { get; set; }
+        public decimal cijena { get; set; }
+    }
+}
