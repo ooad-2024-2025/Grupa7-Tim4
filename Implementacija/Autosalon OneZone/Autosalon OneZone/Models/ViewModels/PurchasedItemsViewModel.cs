942079d073a44ab37a3447addb059d6292222974
hhadzicc <105018985+hhadzicc@users.noreply.github.com>
2025-05-30 11:09:47 +0200

    fix1

---
+++ Implementacija/Autosalon OneZone/Autosalon OneZone/Models/ViewModels/PurchasedItemsViewModel.cs
@@ -0,0 +1,31 @@
+﻿// Models/ViewModels/PurchasedItemsViewModel.cs
+using System;
+using System.Collections.Generic;
+
+namespace Autosalon_OneZone.Models.ViewModels
+{
+    public class PurchasedItemsViewModel
+    {
+        public List<PurchasedItemViewModel> PurchasedItems { get; set; } = new List<PurchasedItemViewModel>();
+
+        public class PurchasedItemViewModel
+        {
+            public int VoziloID { get; set; }
+            public string Naziv { get; set; }
+            public string Slika { get; set; }
+            public decimal Cijena { get; set; }
+            public DateTime DatumKupovine { get; set; }
+            public int? NarudzbaID { get; set; }
+            public RecenzijaViewModel Recenzija { get; set; }
+            public bool HasReview => Recenzija != null;
+        }
+
+        public class RecenzijaViewModel
+        {
+            public int RecenzijaID { get; set; }
+            public int Ocjena { get; set; }
+            public string Komentar { get; set; }
+            public DateTime DatumRecenzije { get; set; }
+        }
+    }
+}
