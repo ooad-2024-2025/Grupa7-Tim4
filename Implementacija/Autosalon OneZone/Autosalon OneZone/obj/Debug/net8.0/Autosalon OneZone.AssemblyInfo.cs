5b71b6ce31851b605c6d8e8424495cb442748a29
hhadzicc <hhadzic4@gmail.com>
2025-05-30 11:01:40 +0200

    Recenzije dodane i dodani su fileovi koji fale nakon clone.?

--- Implementacija/Autosalon OneZone/Autosalon OneZone/obj/Debug/net8.0/Autosalon OneZone.AssemblyInfo.cs
+++ Implementacija/Autosalon OneZone/Autosalon OneZone/obj/Debug/net8.0/Autosalon OneZone.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Autosalon OneZone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90d363936f46ed65514e32dd7e8c2f18f227655b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10cb01856f357becc8bc7de68a9590a48ff62a79")]
 [assembly: System.Reflection.AssemblyProductAttribute("Autosalon OneZone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Autosalon OneZone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
